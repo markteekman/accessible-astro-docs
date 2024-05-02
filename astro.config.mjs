511ecbef6835a1723c31c6094e264534657f5a6f
Amanda Guthrie <51721024+amandaguthrie@users.noreply.github.com>
2024-05-02 15:42:31 -0600

    Add: Starlight, docs structure, component docs template, component content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,54 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  site: 'https://accessible-astro-docs.netlify.app/',
+  integrations: [starlight({
+    title: {
+      en: 'Accessible Astro Docs'
+    },
+    logo: { src: './src/assets/img/logo.svg', alt: 'Accessible Astro Docs', replacesTitle: false },
+    favicon: 'favicon.svg',
+    editLink: { baseUrl: 'https://github.com/markteekman/accessible-astro-docs' },
+    sidebar: [
+      { label: 'Components', autogenerate: { directory: 'components' } },
+      { label: 'Contribute', link: '/contribute' }
+    ],
+    social: {
+      github: 'https://github.com/markteekman/accessible-astro-docs',
+    },
+    defaultLocale: 'root',
+    locales: {
+      root: {
+        label: 'English',
+        lang: 'en'
+      }
+    },
+    head: [
+      // favicon fallback for Safari
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'icon',
+          href: '/favicon.png',
+          sizes: '48x48'
+        }
+      },
+      {
+        tag: 'script',
+        attrs: {
+          src: '/scripts/starlight-accessibility.js',
+        }
+      },
+      {
+        tag: 'script',
+        attrs: {
+          src: 'https://tarptaeya.github.io/repo-card/repo-card.js',
+          defer: true
+        }
+      }
+    ]
+  })]
+});
---
+++ src/content/docs/components/getting-started.md
@@ -0,0 +1,37 @@
+---
+title: Getting Started
+description: Install and import Accessible Astro Components
+sidebar:
+  order: 1
+---
+
+## Overview
+
+A set of Accessible, easy to use, Front-end UI Components for [Astro](https://astro.build/). Most of these components
+are used in the [Accessible
+Astro Starter](https://github.com/markteekman/accessible-astro-starter) and
+the [Accessible Astro Dashboard](https://github.com/markteekman/accessible-astro-dashboard) themes, and they provide
+exemplary use cases for these components. As a
+bonus, these themes also have their own dedicated components (like keyboard accessible and responsive navigations) and
+many **Design System** utility classes, patterns and primitives (such as grids, buttons, lists, spacings, sizes and more).
+
+🚀 [Live preview](https://accessible-astro.netlify.app/accessible-components/)
+
+## Installation
+
+Run the following command in your project folder to get started.
+
+```bash
+npm install accessible-astro-components --save-dev
+```
+
+## Usage
+
+Import the components from the package using the following import statement:
+
+```astro
+---
+import { Accordion, AccordionItem, Card, Modal, ... } from 'accessible-astro-components'
+---
+```
+
